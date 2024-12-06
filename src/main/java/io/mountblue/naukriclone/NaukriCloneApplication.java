a3a54deedc2249de0717087e7fd78abe787b862f
Himanshu Kaushik <himanshu.kaushik127@gmail.com>
2024-12-06 10:33:38 +0530

    Initialized Naukri clone project

---
+++ src/main/java/io/mountblue/naukriclone/NaukriCloneApplication.java
@@ -0,0 +1,13 @@
+package io.mountblue.naukriclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaukriCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NaukriCloneApplication.class, args);
+    }
+
+}
